e7498d07bf03e880db6a868a95b767a32ac4ab78
aalvarado <adan.alvarado7@gmail.com>
2018-08-22 14:22:32 -0700

    Arrays: cyclic rotation

---
+++ l2/cyclic_rotation.js
@@ -0,0 +1,21 @@
+function solution(A, K) {
+  var newList = [];
+
+  if (K > A.length - 1) {
+    K = (K % A.length);
+  }
+
+  if (K == 0) {
+    return A;
+  }
+
+  for (var i = A.length - K; i < A.length; i++){
+    newList.push(A[i])
+  }
+
+  for (var i = 0; i < A.length - K; i++) {
+    newList.push(A[i])
+  }
+
+  return newList;
+}
